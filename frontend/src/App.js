8620c6514ad35a848099b163c027b846b365e579
sai-myne <ahlusir@gmail.com>
2021-08-12 19:06:30 +0800

    Creating react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// page & layout imports
+import Homepage from "./pages/Homepage";
+import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
+import SiteHeader from "./components/SiteHeader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/details/:id">
+            <ReviewDetails />
+          </Route>
+          <Route path="/category/:id">
+            <Category />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
